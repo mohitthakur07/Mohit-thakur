3bde90f2f3031fc10b8716ceecd577f59ece0d0c
Mohjit kumar thakur <mohitkumart19@gmail.com>
2024-08-19 11:11:38 +0530

    message

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,39 +1,66 @@
-// Project.jsx
+import { motion } from "framer-motion";
 import ProjectTab from "./ProjectTab";
-import "./Project.css"; // Assuming you have a CSS file for styling
+import bookHub from "./assets/book-hub.png";
+import weatherImg from "./assets/weather-img.jpg";
+import Wonderlust from "./assets/Wonderlust.jpg";
+import AssignmentIcon from "@mui/icons-material/Assignment";
 
+import "./Project.css";
 export default function Project() {
   const projects = [
     {
       title: "Book Hub",
-      description: "Book Hub connects students to share and explore books.",
-      imageUrl: "/path-to-weather-app-image.jpg",
-      techStack: ["React", "CSS", "API"],
-      projectLink: "https://your-weather-app-link.com",
+      description:
+        "Book Hub is a platform for connecting students and book enthusiasts to share and explore a diverse collection of books.",
+      imageUrl: bookHub,
+      techStack: ["HTML", "CSS", "Js", "ejs", "express.js", "mongo.db"],
+      projectLink: "https://book-hub-9670.onrender.com/",
     },
     {
       title: "SkyCast",
       description:
         "A weather forecasting app built with React and the OpenWeather API.",
-      imageUrl: "/path-to-weather-app-image.jpg",
+      imageUrl: weatherImg,
       techStack: ["React", "CSS", "API"],
-      projectLink: "https://your-weather-app-link.com",
+      projectLink: "https://skyCast.com",
     },
     {
-      title: "Portfolio Website",
+      title: "Wonderlust",
       description:
-        "My personal portfolio website showcasing my projects and skills.",
-      imageUrl: "/path-to-portfolio-image.jpg",
-      techStack: ["React", "CSS", "JavaScript"],
-      projectLink: "https://your-portfolio-link.com",
+        "Wonderlust is a web platform designed to help users find and explore available rooms. This platform allows users to search for rooms, view details, and connect with room owners directly.",
+      imageUrl: Wonderlust,
+      techStack: ["full-stack web platform"],
+      projectLink: "https://github.com/mohitthakur07/Wonderlust",
     },
+    // {
+    //   title: "Portfolio Website",
+    //   description:
+    //     "My personal portfolio website showcasing my projects and skills.",
+    //   imageUrl: "/path-to-portfolio-image.jpg",
+    //   techStack: ["React", "CSS", "JavaScript"],
+    //   projectLink: "https://your-portfolio-link.com",
+    // },
     // Add more projects as needed
   ];
 
   return (
     <div className="projects-section">
-      <h2>Projects</h2>
-      <div className="projects-container">
+      <h2>
+        Projects <AssignmentIcon />
+      </h2>
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{
+          opacity: { duration: 1.5 },
+          delay: 0.2,
+          type: "spring",
+          stiffness: 50,
+          ease: "easeIn",
+          duration: 1,
+        }}
+        className="projects-container"
+      >
         {projects.map((project, index) => (
           <ProjectTab
             key={index}
@@ -44,7 +71,7 @@ export default function Project() {
             projectLink={project.projectLink}
           />
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 }
